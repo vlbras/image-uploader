47d9ad6a0a9bf22b75698ffcd085b0076a2fc176
vlbras <vlad.bras02@gmail.com>
2023-11-29 23:51:58 +0100

    feat: integrate @nestjs/config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT);
+  Logger.log(`Running on port ${process.env.APP_PORT}`, 'Bootstrap');
 }
 bootstrap();
