2310e633e2ef0a909dfbd38c8aa1bff8709df863
vlbras <vlad.bras02@gmail.com>
2023-11-30 11:49:06 +0100

    feat: integrate @nestjs/throttler

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ThrottlerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: config.getOrThrow('THROTTLE_TTL'),
+          limit: config.getOrThrow('THROTTLE_LIMIT'),
+        },
+      ],
+    }),
+  ],
   controllers: [UploadController],
-  providers: [UploadService],
+  providers: [
+    UploadService,
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class UploadModule {}
