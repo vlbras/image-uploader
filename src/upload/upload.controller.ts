7b349bd1a86b1cd72ef53fa43e81fa88f695656b
vlbras <vlad.bras02@gmail.com>
2023-11-30 11:30:16 +0100

    feat: implement upload controller

---
+++ src/upload/upload.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { UploadService } from './upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  upload(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 4 }), // 4MB
+          new FileTypeValidator({ fileType: 'image/(png|jpeg|jpg)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return this.uploadService.upload(file.originalname, file.buffer);
+  }
+}
