3f4d9bcca31399bcf2daafa2ccca871cd50c4e5a
vlbras <vlad.bras02@gmail.com>
2023-11-30 11:30:31 +0100

    feat: integrate upload resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UploadModule],
 })
 export class AppModule {}
